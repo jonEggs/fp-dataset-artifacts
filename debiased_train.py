9c341d5dd594377e7aacdbbddb65f251ef6c6037
largeEgg223 <157918574+jonEggs@users.noreply.github.com>
2025-12-03 17:51:09 -0500

    Update debiased_train.py

--- debiased_train.py
+++ debiased_train.py
@@ -137,10 +137,6 @@ training_args = TrainingArguments(
     output_dir='./debiased_model',
     num_train_epochs=3,
     per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
-    logging_steps=100,
     do_train=True,
     do_eval=True,
 )
