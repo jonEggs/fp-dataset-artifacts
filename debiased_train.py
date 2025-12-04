29dc4acf0bde1bd04b817eb507f1a976e2684ddb
Jonathan <jonj@bu.edu>
2025-12-04 15:53:44 -0500

    Update debiased_train.py

--- debiased_train.py
+++ debiased_train.py
@@ -171,7 +171,7 @@ eval_dataset.set_format(
 
 bias_coefficient = 0.3 # Define bias coefficient once
 training_args = TrainingArguments(
-    output_dir=f'/content/drive/MyDrive/nli_models/debiased_model_{bias_coefficient}',
+    output_dir=f'/content/drive/MyDrive/nli_models/debiased_reweight_model_{bias_coefficient}',
     num_train_epochs=3,
     per_device_train_batch_size=16,
     logging_steps=10000,
